dbce928a3e15f9016020d00a26be69e6e55a6526
Yannick Moris <yanndevpro@gmail.com>
2023-05-01 22:47:09 +0200

    mise en place du JS suite et fin de l'exercice

--- app.js
+++ app.js
@@ -6,36 +6,52 @@ const BMIData = [
   { name: "Obésité sévère", color: "crimson", range: [35, 40] },
   { name: "Obésité morbide", color: "purple", range: 40 },
 ];
-
 // IMC = poids en kg / taille² en m
 
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 
-form.addEventListener("submit", handleform)
+form.addEventListener("submit", handleform);
 
 function handleform(e) {
   e.preventDefault();
-  
   calculateBMI();
 }
 
-const inputs = document.querySelectorAll("input")
+const inputs = document.querySelectorAll("input");
 
 function calculateBMI() {
   const height = inputs[0].value;
   const weight = inputs[1].value;
 
-  if (!height || weight || height <= 0 || weight <= 0) {
-    handleError()
-    return;    
+  if (!height || !weight || height <= 0 || weight <= 0) {
+    handleError();
+    return;
   }
+
+  const BMI = (weight / Math.pow(height / 100, 2)).toFixed(1);
+  console.log(BMI);
+
+  showResult(BMI);
 }
 
 const displayBMI = document.querySelector(".bmi-value");
 const result = document.querySelector(".result");
 
 function handleError() {
-  displayBMI.textContent = "Wops"
-  result.textContent = "Remplissez correctement les inputs"
+  displayBMI.textContent = "Wops";
+  displayBMI.style.color = "inherit";
+  result.textContent = "Remplissez correctement les inputs";
 }
 
+function showResult(BMI) {
+  const rank = BMIData.find(data => {
+    if(BMI >= data.range[0] && BMI < data.range[1]) return data; 
+    else if(typeof data.range === "number" && BMI >= data.range) return data;
+  })
+
+  displayBMI.textContent = BMI;
+
+  displayBMI.style.color = `${rank.color}`
+
+  result.textContent = `resultat : ${rank.name}`
+}
